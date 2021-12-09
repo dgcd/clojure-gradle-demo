a662e75636effad68d0414a91fbf1bbb804660e7
RAJKUMAR NATARAJAN <raj.cs.psg@gmail.com>
2021-12-09 00:06:14 -0500

    initial commit

---
+++ src/main/clojure/math/calc.clj
@@ -0,0 +1,3 @@
+(ns math.calc)
+(defn add [x y] (+ x y))
+(defn demo [x y] (* 2 x y))
---
+++ src/test/clojure/math/calc_test.clj
@@ -0,0 +1,8 @@
+(ns math.calc_test
+  (:require [clojure.test :refer :all]
+            [math.calc :refer :all]))
+
+(deftest add-test
+  (println "adding numbers")
+  (testing "adding two numbers"
+    (is (= 4  (add 1 3)))))
